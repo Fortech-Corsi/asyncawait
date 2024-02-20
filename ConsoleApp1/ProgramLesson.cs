fb747d92127c3874b0688fbe3c657fc23f7d8d8e
Davide Monticelli (Fortech s.r.l.) <davide.monticelli@4ts.it>
2024-02-20 14:47:14 +0100

    aggiunto consegna

--- ConsoleApp1/ProgramLesson.cs
+++ ConsoleApp1/ProgramLesson.cs
@@ -1,53 +1,58 @@
 
-// These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
-using System.Diagnostics;
+// // These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
+// using System.Diagnostics;
 
-internal class HttpCall { }
-internal class BigData { }
-internal class Print { }
+// internal class HttpCall { }
+// internal class BigData { }
+// internal class Print { }
 
-class Program
-{
-    public static async Task Main(string[] args)
-    {
-        Stopwatch s = new Stopwatch();
-        s.Start();
+// class Program
+// {
+//     public static async Task Main(string[] args)
+//     {
+//         Stopwatch s = new Stopwatch();
+//         s.Start();
 
-        Console.WriteLine($"START {s.ElapsedMilliseconds}");
+//         Console.WriteLine($"START {s.ElapsedMilliseconds}");
 
-        Task<HttpCall> httpData = DoHttpCall();
-        BigData bigData = RetrieveBigData();
-        Print print = PrintData();        
+//         // 10s
+//         Task<HttpCall> httpData = DoHttpCall();
+//         // 5s
+//         Task<BigData> bigData = RetrieveBigData();
+        
+//         await Task.WhenAll(httpData, bigData);
 
-        Console.WriteLine($"END {s.ElapsedMilliseconds}");
-    }
+//         Print print = await PrintData();        
+
+//         Console.WriteLine($"END {s.ElapsedMilliseconds}");
+//     }
 
 
-    private static BigData RetrieveBigData()
-    {
-        Console.WriteLine("RetrieveBigData START");
-        Task.Delay(3000).Wait();
-        Console.WriteLine("RetrieveBigData END");
+//     private async static Task<BigData> RetrieveBigData()
+//     {
+//         Console.WriteLine("RetrieveBigData START");
+//         await Task.Delay(5000);
+//         Console.WriteLine("RetrieveBigData END");
 
-        return new BigData();
-    }
+//         return new BigData();
+//     }
 
-    private async static Task<HttpCall> DoHttpCall()
-    {
-        Console.WriteLine("DoHttpCall START");
-        await Task.Delay(3000);
-        Console.WriteLine("DoHttpCall END");
+//     private async static Task<HttpCall> DoHttpCall()
+//     {
+//         Console.WriteLine("DoHttpCall START");
+//         await Task.Delay(100000);
+//         Console.WriteLine("DoHttpCall END");
 
-        return new HttpCall();
-    }
+//         return new HttpCall();
+//     }
 
-    private static Print PrintData()
-    {
-        Console.WriteLine("PrintData START");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("PrintData END");
+//     private async static Task<Print> PrintData()
+//     {
+//         Console.WriteLine("PrintData START");
+//         await Task.Delay(1000);
+//         Console.WriteLine("PrintData END");
 
-        return new Print();
-    }
+//         return new Print();
+//     }
 
-}
+// }
--- ConsoleApp1/ProgramStubTest.cs
+++ ConsoleApp1/ProgramStubTest.cs
@@ -1,20 +1,27 @@
 
-// // These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
-// using System.Diagnostics;
+// These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
+using System.Diagnostics;
 
-// class Program
-// {
-//     public static async Task Main(string[] args)
-//     {
-//         Stopwatch s = new Stopwatch();
-//         s.Start();
+// CREARE METODI:
+// PreparaCaffe -> 3s
+// TostaPane -> 4s
+// ImburraPane -> 4s
+// SpremiLeArance -> 5s
+// ConsumaLaColazione ->6s
 
-//         Console.WriteLine($"START {s.ElapsedMilliseconds}");
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Stopwatch s = new Stopwatch();
+        s.Start();
+
+        Console.WriteLine($"START {s.ElapsedMilliseconds}");
 
         
 
-//         Console.WriteLine($"END {s.ElapsedMilliseconds}");
-//     }
+        Console.WriteLine($"END {s.ElapsedMilliseconds}");
+    }
 
 
-// }
+}

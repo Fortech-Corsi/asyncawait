643b3301c50d58ca556d67d8c7d1fe17183f8534
Davide Monticelli (Fortech s.r.l.) <davide.monticelli@4ts.it>
2024-02-20 13:50:57 +0100

    setup

---
+++ ConsoleApp1/ProgramStubTest.cs
@@ -0,0 +1,20 @@
+
+// // These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
+// using System.Diagnostics;
+
+// class Program
+// {
+//     public static async Task Main(string[] args)
+//     {
+//         Stopwatch s = new Stopwatch();
+//         s.Start();
+
+//         Console.WriteLine($"START {s.ElapsedMilliseconds}");
+
+        
+
+//         Console.WriteLine($"END {s.ElapsedMilliseconds}");
+//     }
+
+
+// }
